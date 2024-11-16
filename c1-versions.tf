e4cf13dcc28a526320952edc2b14100915094df7
rakeshgowd100 <rakeshgowd100@gmail.com>
2024-11-16 21:35:20 +0530

    tjrekej

--- c1-versions.tf
+++ c1-versions.tf
@@ -18,10 +18,10 @@ terraform {
   }
 }
 
-# provider "aws" {
-#   profile = "default"
-#   region  = var.my_region
-# }
+provider "aws" {
+  # profile = "default"
+  region  = var.my_region
+}
 
 # provider "aws" {
 #   profile = "default"
